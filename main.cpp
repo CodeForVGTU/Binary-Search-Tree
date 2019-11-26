0a306dc396671470d07c2837b47aeee61e5d7106
Rimvydas <40775067+RimveBiceps@users.noreply.github.com>
2019-11-26 18:20:50 +0200

    Added Deleting
    Deleting still not working properly. Need to fix it!

--- main.cpp
+++ main.cpp
@@ -106,8 +106,68 @@ int maxDepth(Node* Node)
 	}
 }
 
+// FOR DELETING ELEMENT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+struct Node* minValueNode(struct Node* node)
+{
+	struct Node* current = node;
+
+	/* loop down to find the leftmost leaf */
+	while (current && current->left != NULL)
+		current = current->left;
+
+	return current;
+}
+
+//-----------------------
+struct Node* deleteNode(struct Node* root, int key)
+{
+	// base case 
+	if (root == NULL) return root;
+
+	// If the key to be deleted is smaller than the root's key, 
+	// then it lies in left subtree 
+	if (key < root->data)
+		root->left = deleteNode(root->left, key);
+
+	// If the key to be deleted is greater than the root's key, 
+	// then it lies in right subtree 
+	else if (key > root->data)
+		root->right = deleteNode(root->right, key);
+
+	// if key is same as root's key, then This is the node 
+	// to be deleted 
+	else
+	{
+		// node with only one child or no child 
+		if (root->left == NULL)
+		{
+			struct Node* temp = root->right;
+			free(root);
+			return temp;
+		}
+		else if (root->right == NULL)
+		{
+			struct Node* temp = root->left;
+			free(root);
+			return temp;
+		}
+
+		// node with two children: Get the inorder successor (smallest 
+		// in the right subtree) 
+		struct Node* temp = minValueNode(root->right);
+
+		// Copy the inorder successor's content to this node 
+		root->data = temp->data;
+
+		// Delete the inorder successor 
+		root->right = deleteNode(root->right, temp->data);
+	}
+	return root;
+}
+// END OF DELETING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 void Menu() {
-  int choice = 0, size = 0, find_number;
+  int choice = 0, size = 0, input_number;
   string txtname;
   struct Node *root = NULL;
 
@@ -178,16 +238,19 @@ void Menu() {
     }
 		else if (choice == 2)
     {
-			cout << "Delete element from Binary Tree";
+			cout << "Delete element from Binary Tree\nEnter number: ";
+      
+      cin >> input_number;
+      deleteNode(root, input_number);
     }
 		else if (choice == 3)
     {
-			cout << "Find element in Binary Tree\n";
+			cout << "Find element in Binary Tree\nEnter number: ";
 
-      cin >> find_number;
-      if (ifNodeExists(root, find_number))
+      cin >> input_number;
+      if (ifNodeExists(root, input_number))
       {
-        cout << "Number " << find_number << " was found!\n";
+        cout << "Number " << input_number << " was found!\n";
       }
     	else { cout << "Number doesn't exist.. \n"; }
     }
